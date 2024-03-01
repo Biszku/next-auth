64ffbd26e25e7611e7b27c13e6e5d05ae8127591
Biszku <dbichu@onet.pl>
2024-03-01 18:28:00 +0100

    created changePasswordHandler

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,31 +1,24 @@
-import { getSession } from "next-auth/react";
-import { useState, useEffect } from "react";
-
 import ProfileForm from "./profile-form";
 
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
+  const changePasswordHandler = async (passwordData) => {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-  }, []);
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+    const data = await response.json();
+  };
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
