9edb9a5f53b5c7f0633ad17488ef039aaa284135
Biszku <dbichu@onet.pl>
2024-03-01 15:24:48 +0100

    implemented login

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -44,7 +44,10 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword,
       });
-      console.log(result);
+
+      if (!result.error) {
+        // set some auth state
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
